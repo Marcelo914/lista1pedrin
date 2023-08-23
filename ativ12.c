372f1cb53bf01e522cf5d606876175f5a81fa8a5
Marcelo914 <marcelosouza222244@gmail.com>
2023-08-23 18:52:57 -0300

    update

--- ativ12.c
+++ ativ12.c
@@ -2,6 +2,12 @@
 
 int main(void)
 {
-  
+  int num, maior, menor;
+
+
+  printf("Digite numeros");
+
+  while () {
+  }
   return 0;
 }
--- ativ5.c
+++ ativ5.c
@@ -2,20 +2,20 @@
 
 int main(void)
 {
-  int num1, num2, divisao;
+  int num1, num2;
+  float divisao;
 
   printf("Digite o número 1:");
-  printf("Digite o número 2:");
-
   scanf("%i", &num1);
+  printf("Digite o número 2:");
   scanf("%i", &num2);
 
   if (num2 == 0) {
     printf("Essa operação não é possivel");
   }
   else {
-    divisao = num1/num2;
-    printf("%d", divisao);
+    divisao = (float)num1/num2;
+    printf("%.1f", divisao);
   }
   return 0;
 }
