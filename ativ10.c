273077cd01ba1cecd0efa7af405b4a52cd22724b
Marcelo914 <marcelosouza222244@gmail.com>
2023-08-23 15:13:26 -0300

    ativ10

--- ativ10.c
+++ ativ10.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 
-int main(void)
-{
-  printf("ḿn")
+int main(void) {
+  float product;
+  char pais;
+  float frete;
+  float totalAPagar;
+
+  printf("Qual o valor do produto\n");
+  scanf("%f", &product);
+
+  printf("país - frete\n");
+  printf("Estados Unidos(e) - 60,00\nFrança(f)- 75,00\nMéxico(m) - "
+         "50,00\nArgentina(a) - 27,35\nChina(c) - 80,00\n");
+  printf("Para onde será enviado?(apenas letra)\n");
+  scanf("%s", &pais);
+
+  switch (pais) {
+  case 'e':
+    frete = 60;
+    totalAPagar = product + frete;
+    printf("%.2f\n", totalAPagar);
+    break;
+
+  case 'f':
+    frete = 75;
+    totalAPagar = product + frete;
+    printf("%.2f\n", totalAPagar);
+    break;
+
+  case 'm':
+    frete = 50;
+    totalAPagar = product + frete;
+    printf("%.2f\n", totalAPagar);
+    break;
+
+  case 'a':
+    frete = 27.35;
+    totalAPagar = product + frete;
+    printf("%.2f\n", totalAPagar);
+    break;
+
+  case 'c':
+    frete = 80;
+    totalAPagar = product + frete;
+    printf("%.2f\n", totalAPagar);
+    break;
+  }
+
   return 0;
 }
